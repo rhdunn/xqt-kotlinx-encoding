df0c54235bf18a12ceb583cfddd23f3b773878f3
Reece H. Dunn <msclrhd@gmail.com>
2022-11-04 22:53:14 +0000

    Support Latin1 character sequences.

---
+++ src/kotlinx-encoding/commonMain/kotlinx/encoding/decoder/Latin1CharSequence.kt
@@ -0,0 +1,37 @@
+// Copyright (C) 2022 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package kotlinx.encoding.decoder
+
+import kotlin.experimental.and
+
+/**
+ * Creates an ASCII/Latin-1 character sequence over a byte array.
+ *
+ * Note that this is not the windows-1252 remapping of ISO-8859-1 as specified
+ * in the WHATWG Encoding specification for HTML5, but is instead the first 256
+ * Unicode characters per the original ISO-8859-1 codepage.
+ *
+ * @param data The underlying byte array.
+ * @param offset The offset of the first byte to read from.
+ * @param length The number of bytes in this sequence.
+ */
+class Latin1CharSequence private constructor(
+    private val data: ByteArray,
+    private val offset: Int,
+    override val length: Int
+) : CharSequence {
+    constructor(data: ByteArray) : this(data, 0, data.size)
+
+    override fun get(index: Int): Char = when {
+        index < 0 || index >= length -> throw IndexOutOfBoundsException(index.toString())
+        else -> (data[offset + index] and 0xFF.toByte()).toInt().toChar()
+    }
+
+    override fun subSequence(startIndex: Int, endIndex: Int): CharSequence = when {
+        startIndex < 0 -> throw IndexOutOfBoundsException(startIndex.toString())
+        endIndex > length -> throw IndexOutOfBoundsException(endIndex.toString())
+        startIndex > endIndex -> throw IllegalArgumentException("$startIndex > $endIndex")
+        else -> Latin1CharSequence(data, offset + startIndex, endIndex - startIndex)
+    }
+
+    override fun toString(): String = CharArray(length) { i -> this[i] }.concatToString()
+}
---
+++ src/kotlinx-encoding/commonTest/kotlinx/encoding/test/decoder/Latin1CharSequence.kt
@@ -0,0 +1,109 @@
+// Copyright (C) 2022 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package kotlinx.encoding.test.decoder
+
+import kotlinx.encoding.decoder.Latin1CharSequence
+import kotlinx.test.DisplayName
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+@Suppress("JoinDeclarationAndAssignment")
+@DisplayName("The ASCII/Latin 1 CharSequence decoder")
+class TheAsciiLatin1CharSequenceDecoder {
+    @Test
+    @DisplayName("can be constructed from a byte sequence")
+    fun can_be_constructed_from_a_byte_sequence() {
+        val data = byteArrayOf(0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39)
+        val seq = Latin1CharSequence(data)
+
+        assertEquals(10, seq.length)
+        assertEquals("0123456789", seq.toString())
+
+        assertEquals('0', seq[0])
+        assertEquals('9', seq[9])
+    }
+
+    @Test
+    @DisplayName("can return subsequences of the original sequence")
+    fun can_return_subsequences_of_the_original_sequence() {
+        val data = byteArrayOf(0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39)
+        val seq1 = Latin1CharSequence(data)
+        val seq2 = seq1.subSequence(2, 8)
+
+        assertEquals(6, seq2.length)
+        assertEquals("234567", seq2.toString())
+
+        assertEquals('2', seq2[0])
+        assertEquals('7', seq2[5])
+
+        assertEquals(0, seq1.subSequence(10, 10).length)
+        assertEquals("", seq1.subSequence(10, 10).toString())
+    }
+
+    @Test
+    @DisplayName("can return subsequences of a subsequence")
+    fun can_return_subsequences_of_a_subsequence() {
+        val data = byteArrayOf(0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39)
+        val seq1 = Latin1CharSequence(data)
+        val seq2 = seq1.subSequence(2, 8)
+        val seq3 = seq2.subSequence(2, 5)
+
+        assertEquals(3, seq3.length)
+        assertEquals("456", seq3.toString())
+
+        assertEquals('4', seq3[0])
+        assertEquals('6', seq3[2])
+    }
+
+    @Test
+    @DisplayName("throws IndexOutOfBoundsException for invalid subsequence arguments")
+    fun throws_index_out_of_bounds_exception_for_invalid_subsequence_arguments() {
+        val data = byteArrayOf(0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39)
+        val seq = Latin1CharSequence(data)
+        var e: Throwable
+
+        e = assertFailsWith<IndexOutOfBoundsException> { seq.subSequence(-1, 0) }
+        assertEquals("-1", e.message)
+
+        e = assertFailsWith<IndexOutOfBoundsException> { seq.subSequence(11, 11) }
+        assertEquals("11", e.message)
+
+        e = assertFailsWith<IndexOutOfBoundsException> { seq.subSequence(0, 11) }
+        assertEquals("11", e.message)
+
+        e = assertFailsWith<IndexOutOfBoundsException> { seq.subSequence(1, 11) }
+        assertEquals("11", e.message)
+
+        e = assertFailsWith<IllegalArgumentException> { seq.subSequence(6, 4) }
+        assertEquals("6 > 4", e.message)
+    }
+
+    @Test
+    @DisplayName("throws IndexOutOfBoundsException for invalid indices")
+    fun throws_index_out_of_bounds_exception_for_invalid_indices() {
+        val data = byteArrayOf(0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39)
+        val seq = Latin1CharSequence(data)
+        var e: IndexOutOfBoundsException
+
+        e = assertFailsWith { seq[-1] }
+        assertEquals("-1", e.message)
+
+        e = assertFailsWith { seq[10] }
+        assertEquals("10", e.message)
+    }
+
+    @Test
+    @DisplayName("throws IndexOutOfBoundsException for invalid indices on subsequences")
+    fun throws_index_out_of_bounds_exception_for_invalid_indices_on_subsequences() {
+        val data = byteArrayOf(0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39)
+        val seq1 = Latin1CharSequence(data)
+        val seq2 = seq1.subSequence(2, 8)
+        var e: IndexOutOfBoundsException
+
+        e = assertFailsWith { seq2[-1] }
+        assertEquals("-1", e.message)
+
+        e = assertFailsWith { seq2[6] }
+        assertEquals("6", e.message)
+    }
+}
