rhdunn/xqt-platform

Create a top-level gradle project.

settings.gradle.kts
@@ -1,0 +1,1 @@
+rootProject.name = "xqt-platform"

rhdunn/xqt-kotlinx-encoding

settings.gradle.kts
@@ -1,0 +1,1 @@
+rootProject.name = "kotlinx-encoding"