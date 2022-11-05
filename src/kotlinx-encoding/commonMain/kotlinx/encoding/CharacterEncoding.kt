e94ceaefb3607b2e9bdfa2c24bfdb4396a9f5863
Reece H. Dunn <msclrhd@gmail.com>
2022-11-05 07:57:58 +0000

    IANA ISO-8859-1 character set.

--- src/kotlinx-encoding/commonMain/kotlinx/encoding/CharacterEncoding.kt
+++ src/kotlinx-encoding/commonMain/kotlinx/encoding/CharacterEncoding.kt
@@ -11,6 +11,12 @@ package kotlinx.encoding
  */
 enum class CharacterEncoding(val mimeName: String, val ianaName: String) {
     /**
+     * The latin 1 character encoding.
+     *
+     * @see <a href="https://www.iana.org/go/rfc1345">RFC1345</a>
+     */
+    ISO_8859_1("ISO-8859-1", "ISO-8859-1:1987"),
+    /**
      * The ANSI X3.4-1986 character encoding.
      *
      * @see <a href="https://www.iana.org/go/rfc2046">RFC2046</a>
@@ -28,11 +34,19 @@ enum class CharacterEncoding(val mimeName: String, val ianaName: String) {
             "ANSI_X3.4-1968" to US_ASCII,
             "ANSI_X3.4-1986" to US_ASCII,
             "cp367" to US_ASCII,
+            "CP819" to ISO_8859_1,
             "csASCII" to US_ASCII,
+            "csISOLatin1" to ISO_8859_1,
             "IBM367" to US_ASCII,
+            "IBM819" to ISO_8859_1,
             "ISO_646.irv:1991" to US_ASCII,
+            "ISO_8859-1" to ISO_8859_1,
+            "ISO-8859-1" to ISO_8859_1,
             "iso-ir-6" to US_ASCII,
+            "iso-ir-100" to ISO_8859_1,
             "ISO646-US" to US_ASCII,
+            "latin1" to ISO_8859_1,
+            "l1" to ISO_8859_1,
             "us" to US_ASCII,
             "US-ASCII" to US_ASCII,
         )
